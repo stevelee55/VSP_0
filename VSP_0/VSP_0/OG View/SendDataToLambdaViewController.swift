ee912592e0ec461e7d88bf6213d8d4a692311815
stevesl <stevesl@umich.edu>
2018-05-28 17:14:59 -0400

    Lambda setup almost complete. The current problem is that when I try to connect to lambda using the invoke function, an error regarding the plist shows up. However, the AWS SDK is setup well and I can get the response from it.

--- VSP_0/VSP_0/OG View/SendDataToLambdaViewController.swift
+++ VSP_0/VSP_0/OG View/SendDataToLambdaViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AWSLambda
 
 class SendDataToLambdaViewController: UIViewController {
     
@@ -14,15 +15,35 @@ class SendDataToLambdaViewController: UIViewController {
     
     override func viewDidLoad() {
         
-        //AWS Lambda Setup.
-        
     }
     
     
     //Connecting to AWS Lambda and requesting data/calling function.
     @IBAction func sendDataToAWSLambda(_ sender: Any) {
         //Send data to lambda function.
-        
+        //AWS Lambda Setup.
+        let lambdaInvoker = AWSLambdaInvoker.default()
+        let jsonObject: [String: Any] = ["key1" : "value1",
+                                         "key2" : 2 ,
+                                         "key3" : [1, 2],
+                                         "isError" : false]
+        lambdaInvoker.invokeFunction("randomNumberGenerator", jsonObject: jsonObject).continueWith(block: {(task:AWSTask<AnyObject>) -> Any? in
+            if let error = task.error as NSError? {
+                if (error.domain == AWSLambdaInvokerErrorDomain) && (AWSLambdaInvokerErrorType.functionError == AWSLambdaInvokerErrorType(rawValue: error.code)) {
+                    print("Function error: \(String(describing: error.userInfo[AWSLambdaInvokerFunctionErrorKey]))")
+                    } else {
+                    print("Error: \(error)")
+                    }
+                    return nil
+            }
+            
+            // Handle response in task.result
+            if let JSONDictionary = task.result as? NSDictionary {
+                print("Result: \(JSONDictionary)")
+                print("resultKey: \(String(describing: JSONDictionary["resultKey"]))")
+            }
+            return nil
+        })
     }
 
     //Dismissing the current view controller.
